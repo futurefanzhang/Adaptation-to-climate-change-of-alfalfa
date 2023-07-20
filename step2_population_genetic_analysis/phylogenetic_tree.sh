723ce271e3508a22d517aa6479db27019a41031f
futurefanzhang <61524715+futurefanzhang@users.noreply.github.com>
2023-07-20 15:14:36 +0800

    Update phylogenetic_tree.sh

--- step2_population_genetic_analysis/phylogenetic_tree.sh
+++ step2_population_genetic_analysis/phylogenetic_tree.sh
@@ -1,3 +1,4 @@
 ###step1, generate population phylogenetic tree
-plink --allow-extra-chr --threads 2 --vcf 243_migrate_accession_reorder.vcf --chr 1  --recode vcf --out 243_migrate_accession_reorder_chr1
-python vcf2other.py -i all_712_iqtree_plink_vcf.vcf -o SRR12686087_SRR12686087 -f #-o是设置外类群
+plink --allow-extra-chr --threads 2 --vcf all_population_SNP_filter_missing0.2_meanDP2-50_allele2.vcf  --recode vcf --out all_712_iqtree_plink_vcf  ##use plink generate simple format vcf file
+python vcf2other.py -i all_712_iqtree_plink_vcf.vcf -o SRR12686087_SRR12686087 -f #-o set out group, -f  Write a FASTA matrix
+
