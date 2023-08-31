ecc5c6f6ac6870719eaf171e1526ab8090a018b4
futurefanzhang <61524715+futurefanzhang@users.noreply.github.com>
2023-08-31 13:24:03 +0800

    Update deleterious_variance.sh

--- step3_population_dynamics_analysis/deleterious_variance.sh
+++ step3_population_dynamics_analysis/deleterious_variance.sh
@@ -15,7 +15,7 @@ perl make-SIFT-db-all.pl -config ./zm4_chr1/alfalfa_config_chr1.txt #generate al
 
 ##step 2, move all chr*.gz, chr*regions, and chr*_SIFTDB_stats.txt to one folder
 /public/agis/zhouyongfeng_group/caoshuo/tools/plink/plink --vcf merge_712_allele2_meanDP3-62_miss0.2_addid.plink.vcf --keep 267_accession.txt --allow-extra-chr --recode vcf --out merge_267_allele2_meanDP3-62_miss0.2_addid.plink --threads 1 ##挑选需要分析的个体
-python /public/home/zhangtianhao/soft/superSFS/superSFS.py 1 10truncatula_outgroup.txt 3 merge_267_allele2_meanDP3-62_miss0.2_addid.plink.vcf merge_267_allele2_meanDP3-62_miss0.2_addid.plink_reverse.vcf  ###功能1，根据外类群反转vcf；10truncatula_outgroup.txt是外类群样本名，3代表10个外类群（20个单倍型）中有三个单倍型就翻转SNP，
+python /public/home/zhangtianhao/soft/superSFS/superSFS.py 1 10truncatula_outgroup.txt 11 merge_267_allele2_meanDP3-62_miss0.2_addid.plink.vcf merge_267_allele2_meanDP3-62_miss0.2_addid.plink_reverse.vcf  ###功能1，根据外类群反转vcf；10truncatula_outgroup.txt是外类群样本名，11代表10个外类群（20个单倍型）中超过50%单倍型不一致就翻转SNP，
 java -Djava.awt.headless=true -jar /public/agis/zhouyongfeng_group/zhangfan02/arm_software/SIFT4G_Annotator.jar -c -i merge_267_allele2_meanDP3-62_miss0.2_addid.plink_reverse.vcf -d zm4_database/ -r ./results/ -t #输入vcf文件，-d就是SIFT的文件夹， -r是结果输出路径， -c代表命令行运行，-t表示从多个转录本提取注释信息
 
 ##step 3, extract deleterious SNP and change to R format
@@ -23,7 +23,8 @@ grep "DELETERIOUS" merge_267_allele2_meanDP3-62_miss0.2_addid.plink_reverse_SIFT
 head -n 100 merge_267_allele2_meanDP3-62_miss0.2_addid.plink_reverse_SIFTpredictions.vcf> head100.txt #把vcf的表头加上,（这块我用了三步，有更便捷方法请告诉我）
 grep '^#' head100.txt >head_info.txt
 cat head_info.txt deleterious_vcf_file.vcf > deleterious_vcf_file_all.vcf ##完整的有害变异vcf文件
-perl /public/agis/zhouyongfeng_group/zhangfan02/vcf_file/SIFT4G_zm4_database/change_vcf_format_genotype.pl deleterious_vcf_file_all.vcf > deleterious_vcf_file_all_dosage.vcf ##把0/0,0/1,1/1转换成0,1,2格式
+sed -e "s/\r//g" deleterious_vcf_file_all.vcf > deleterious_vcf_file_all2.vcf ##remove ^M sign of vcf file
+perl /public/agis/zhouyongfeng_group/zhangfan02/vcf_file/SIFT4G_zm4_database/change_vcf_format_genotype.pl deleterious_vcf_file_all2.vcf > deleterious_vcf_file_all_dosage.vcf ##把0/0,0/1,1/1转换成0,1,2格式
 cut -f3,10- deleterious_vcf_file_all_dosage.vcf > R_use_format.vcf #去掉不需要的列
 grep -v '^#' R_use_format.vcf >R_use_format1.vcf #去掉注释信息，生成模R需要用的文件
 
