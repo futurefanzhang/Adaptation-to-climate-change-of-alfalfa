5b6bac7f9c01758db70ff018d2b71a1cfec3d4af
futurefanzhang <61524715+futurefanzhang@users.noreply.github.com>
2023-08-31 13:25:26 +0800

    Update change_vcf_format_genotype.pl

--- step3_population_dynamics_analysis/change_vcf_format_genotype.pl
+++ step3_population_dynamics_analysis/change_vcf_format_genotype.pl
@@ -15,12 +15,26 @@ while (<>) {
   for (my $i=9; $i <= $#a; $i++) {
     if ($a[$i] eq "0/0") {
       $a[$i] = "0";
-    } elsif ($a[$i] eq "1/1") {
+    } 
+ elsif ($a[$i] eq "0|0") {
+      $a[$i] = "0";}
+	elsif ($a[$i] eq "1/1") {
       $a[$i] = "2";
-    } elsif ($a[$i] eq "0/1") {
+    } 
+  elsif ($a[$i] eq "1|1") {
+      $a[$i] = "2";
+    } 
+ elsif ($a[$i] eq "0|1") {
       $a[$i] = "1";
-        } elsif ($a[$i] eq "1/0") {
+    }
+ elsif ($a[$i] eq "1|0") {
       $a[$i] = "1";
+    }
+
+elsif ($a[$i] eq "0/1") {
+      $a[$i] = "1";
+	} elsif ($a[$i] eq "1/0") {
+      $a[$i] = "1";  
     } else {
       $a[$i] = "NA";
     }
