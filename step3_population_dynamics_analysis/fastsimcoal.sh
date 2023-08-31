dc15aeb3fab88f349a3925b2b05b5c747496f8e4
futurefanzhang <61524715+futurefanzhang@users.noreply.github.com>
2023-08-31 13:26:58 +0800

    Update fastsimcoal.sh

--- step3_population_dynamics_analysis/fastsimcoal.sh
+++ step3_population_dynamics_analysis/fastsimcoal.sh
@@ -2,8 +2,8 @@
 java -Xmx10g -jar /public/agis/zhouyongfeng_group/zhangfan02/vcf_file/beagle.22Jul22.46e.jar nthreads=10 gt=merge_51_sample_caerulea_falcata_diploid_outgroup_allele2_meanDP3-62_miss0.2_addid_plink.vcf out=merge_51_sample_caerulea_falcata_diploid_outgroup_allele2_meanDP3-62_miss0.2_addid_plink_beagle.vcf #use beagle for impute
 gunzip merge_51_sample_caerulea_falcata_diploid_outgroup_allele2_meanDP3-62_miss0.2_addid_plink_beagle.vcf.vcf.gz
 ###refer https://github.com/xhchauvet/superSFS for superSFS.py
-python /public/home/zhangtianhao/soft/superSFS/superSFS.py 1 10fastsimcoal_outgroup.txt 3 merge_51_sample_caerulea_falcata_diploid_outgroup_allele2_meanDP3-62_miss0.2_addid_plink_beagle.vcf.vcf merge_51_sample_caerulea_falcata_diploid_outgroup_allele2_meanDP3-62_miss0.2_addid_plink_beagle_reverse.vcf  
-###function 1，speculate ancestory allele of vcf file by outgroup；10fastsimcoal_outgroup.txt is outgroup，3 means if there are 3 alleles of outgroup (total 20 alleles among 10 outgroup), the allele will be transfer by outgroup (from ref to alter or from alter to ref)，
+python /public/home/zhangtianhao/soft/superSFS/superSFS.py 1 10fastsimcoal_outgroup.txt 11 merge_51_sample_caerulea_falcata_diploid_outgroup_allele2_meanDP3-62_miss0.2_addid_plink_beagle.vcf.vcf merge_51_sample_caerulea_falcata_diploid_outgroup_allele2_meanDP3-62_miss0.2_addid_plink_beagle_reverse.vcf  
+###function 1，speculate ancestory allele of vcf file by outgroup；10fastsimcoal_outgroup.txt is outgroup，11 means if there are 11 alleles of outgroup (more than 50%, total 20 alleles among 10 outgroup), the allele will be transfer by outgroup (from ref to alter or from alter to ref)，
 python /public/home/zhangtianhao/soft/superSFS/superSFS.py 2 51_sample_group_fastsimcoal.txt merge_51_sample_caerulea_falcata_diploid_outgroup_allele2_meanDP3-62_miss0.2_addid_plink_beagle_reverse.vcf ./count ##function 2, generate count value for fastsimcoal
 
 python 2psfs.py ##change count to SFS file
